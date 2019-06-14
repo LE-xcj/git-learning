39ebab9f5514f58a4c3b354347cd90e4a7acb6a2
LE-xcj <865263907@qq.com>
2019-06-14 21:49:31 +0800

    user的业务接口定义

--- git-use/src/main/java/com/xc/gituse/service/UserService.java
+++ git-use/src/main/java/com/xc/gituse/service/UserService.java
@@ -1,11 +1,20 @@
 package com.xc.gituse.service;
 
+import com.xc.gituse.entity.User;
+
 /**
  * @author chujian
  * @ClassName UserService
  * @Description 功能描述
  * @date 2019/6/13 22:00
  */
-public class UserService {
+public interface UserService {
+
+    int createUser(User user) throws Exception;
+
+    int deleteUser(Integer id) throws Exception;
+
+    User queryUserById(Integer id) throws Exception;
+
 }
     
---
+++ git-use/src/main/java/com/xc/gituse/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.xc.gituse.service.impl;
+
+import com.xc.gituse.entity.User;
+import com.xc.gituse.service.UserService;
+
+/**
+ * @author chujian
+ * @ClassName UserServiceImpl
+ * @Description 功能描述
+ * @date 2019/6/14 21:48
+ */
+public class UserServiceImpl implements UserService{
+    @Override
+    public int createUser(User user) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int deleteUser(Integer id) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public User queryUserById(Integer id) throws Exception {
+        return null;
+    }
+}
+    
