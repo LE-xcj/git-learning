83e3a349db6c70199e68e864e72d52491af35315
LE-xcj <865263907@qq.com>
2019-06-14 21:54:35 +0800

    order模块的创建

---
+++ git-use/src/main/java/com/xc/gituse/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.xc.gituse.controller;
+
+/**
+ * @author chujian
+ * @ClassName OrderController
+ * @Description 功能描述
+ * @date 2019/6/14 21:53
+ */
+public class OrderController {
+}
+    
---
+++ git-use/src/main/java/com/xc/gituse/entity/Order.java
@@ -0,0 +1,30 @@
+package com.xc.gituse.entity;
+
+/**
+ * @author chujian
+ * @ClassName Order
+ * @Description 功能描述
+ * @date 2019/6/14 21:53
+ */
+public class Order {
+    private Integer id;
+
+    private Integer uid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+}
+    
